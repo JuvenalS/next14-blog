0fe4857fd1113b1abc094832338d1dc5f8f8e346
Juvenal_Silveira <86546205+JuvenalS@users.noreply.github.com>
2024-04-30 12:55:48 +0000

    components props, passing data

--- app/page.js
+++ app/page.js
@@ -1,18 +1,35 @@
-function Card({ text }) {
-  return <div className="border rounded-md border-gray-600 p-4">{text}</div>;
-}
+"use client";
+import { useState } from "react";
+import Card from "@/components/card";
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [names, setNames] = useState(["Pioty", "John", "Terry"]);
+
   const name = "Piotr";
+
+  const handleClickVisible = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleAdd = () => {
+    setNames([...names, "New element!"])
+  }
+
+  const cards =
+    isVisible && names.map((name, index) => <Card key={index}>{name}</Card>);
 
   return (
     <>
       <div className="p-20 space-y-4">
         <div>Hello, {name}</div>
-        <Card text="This is being passed from the parent" />
-        <Card />
-        <Card />
-        <Card />
+        {cards}
+        <div className="flex space-x-4">
+          <button onClick={handleClickVisible}>
+            {isVisible ? "Hide" : "Show"}
+          </button>
+          <button onClick={handleAdd}>Add</button>
+        </div>
       </div>
     </>
   );
